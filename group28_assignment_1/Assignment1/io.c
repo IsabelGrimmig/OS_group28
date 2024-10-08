acf78588ce839b1f102505e18d7c3ad6b57b0c5d
PandarenQt <Cheater.um@gmail.com>
2024-10-08 08:54:45 +0200

    fixed Write_int again

--- group28_assignment_1/Assignment1/io.c
+++ group28_assignment_1/Assignment1/io.c
@@ -42,6 +42,40 @@ int write_string(char* s) {
 
 // Writes n to stdout (without any formatting). If no errors occur, it returns 0, otherwise EOF
 int write_int(int n) {
+    int n:
+    char buffer[size]
+
+    int index = 0
+
+    if n == 0:
+        buffer[0] = '0'
+        buffer[1] = '\0'
+        return write_string(buffer)
+
+    if negative:
+        buffer[index++] = '-'
+        n = -n
+
+    int first_digit = index
+
+    while (n > 0):
+        buffer[index++] = (char) (n%10)
+        n /= 10
+
+    int last_digit = index
+
+//reverse the list, without the start
+    for (int i = 0; i < last_digit, i++):
+        int p1 = i + first_digit
+        int p2 = index--
+        char temp = buffer[p1]
+        buffer[p1] = buffer[p2]
+        buffer[p2] = temp
+
+    buffer[last_digit + 1] = '/0'
+    return write_string(buffer)
+
+/*
     char buffer[12] = {'\0'};    // Buffer large enough to hold an integer, assuming no more than 11 digits + null terminator
         int length = 0;
         
@@ -64,11 +98,11 @@ int write_int(int n) {
                 char temp = buffer[i];
                 buffer[i] = buffer [length - 1];
                 buffer[length - 1] = temp;
-                length--;
             }
             
         }
         //buffer[length] = '\0';              // Null-terminate the string
         return write_string(buffer);        // Use write_string to write it to stdout
+*/
   
 }
